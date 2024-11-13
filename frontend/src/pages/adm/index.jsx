5a4afbfb865bec4ca9b884ce53ce4c2f26689340
luiz4444444 <alvesluiz547@gmail.com>
2024-11-13 09:50:01 -0300

    adm

--- frontend/src/pages/adm/index.jsx
+++ frontend/src/pages/adm/index.jsx
@@ -9,21 +9,27 @@ export default function Adm() {
                 <div className='cabeçalho'>
                     <img src="/assets/images/adm/logo-adm.png" alt="logo" />
                     <h1>Painel</h1>
-                    <p>sair</p>
+                    <Link to='/'><img src="/assets/images/adm/saida.png" alt="sair" className='sair'/></Link>
                 </div>
 
                 <div className='painel'>
                     <h1>Resumo Geral</h1>
                     <img src="/assets/images/adm/grafico-histograma.png" alt="resumo-geral" className='icone'/>
 
-                    <h1>Catálogo</h1>
+                    <h1>Serviços</h1>
                     <img src="/assets/images/adm/aplicativos.png" alt="catalogo" className='icone'/>
 
                     <h1>Pagamentos</h1>
                     <img src="/assets/images/adm/dolar.png" alt="pagamentos" className='icone'/>
 
+                    <h1>Agenda</h1>
+                    <img src="/assets/images/adm/calendario.png" alt="agenda" className='icone'/>
+
+                    <h1>Clientes</h1>
+                    <img src="/assets/images/adm/usuarios.png" alt="clientes" className='icone'/>
+
                     <h1>Profissionais</h1>
-                    
+                    <img src="/assets/images/adm/usuarios.png" alt="clientes" className='icone'/>
                 </div>
             </div>
         </div>
