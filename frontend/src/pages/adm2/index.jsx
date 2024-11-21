35d88cdcad9baecb845331a424bd3bd55b4eb6aa
luiz <alvesluiz547@gmail.com>
2024-11-21 18:22:59 -0300

    feito

--- frontend/src/pages/adm2/index.jsx
+++ frontend/src/pages/adm2/index.jsx
@@ -52,6 +52,20 @@ export default function Adm2(){
                     <option value="Corte & Limpeza de Pele">Corte & Limpeza de Pele</option>
                     <option value="Corte e Barba e Limpeza de Pele">Corte e Barba e Limpeza de Pele</option>
                 </select>
+                <div className="horas">
+                    <button type="button" onClick={() => setTime('08:00')}>08:00</button>
+                    <button type="button" onClick={() => setTime('09:00')}>09:00</button>
+                    <button type="button" onClick={() => setTime('10:00')}>10:00</button>
+                    <button type="button" onClick={() => setTime('11:00')}>11:00</button>
+                    <button type="button" onClick={() => setTime('12:00')}>12:00</button>
+                    <button type="button" onClick={() => setTime('13:00')}>13:00</button>
+                    <button type="button" onClick={() => setTime('14:00')}>14:00</button>
+                    <button type="button" onClick={() => setTime('15:00')}>15:00</button>
+                    <button type="button" onClick={() => setTime('16:00')}>16:00</button>
+                    <button type="button" onClick={() => setTime('17:00')}>17:00</button>
+                    <button type="button" onClick={() => setTime('18:00')}>18:00</button>
+                    <button type="button" onClick={() => setTime('19:00')}>19:00</button>
+                </div>
                 <button type='button'>cadastrar</button>
             </form>
 
