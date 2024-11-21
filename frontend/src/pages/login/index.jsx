0b12628a32b142082ad7781e7e6fa45d6197db09
vegezika46 <ra08562337579@acaonsfatima.org.br>
2024-11-21 10:43:33 -0300

    login

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 export default function () {
 
-    return(
+    return (
 
         <div className='pagina-login pagina'>
             <div className='corpo'>
-                
+
 
                 <div className='direita'>
                     <div className='esquerda'>
@@ -16,14 +16,17 @@ export default function () {
                         <Link to="/login" className='botao-login'>Criar Conta</Link>
                     </div>
                     <div className='posicao'>
-                        <h1>Faça login</h1>
-                        <Link to="/login" className='botao-login'>Usuário</Link>
-                        <Link to="/login" className='botao-login'>Senha</Link>
+                        <form>
+                            <h1>Faça login</h1>
+                            <input name='nome' placeholder="Usuário" type="text" />
+                            <input name='senha' placeholder="Senha" type="password" />
+                            <button type='button'>Cadastrar</button>
+                        </form>
                     </div>
-                    
+
                 </div>
             </div>
-            
+
         </div>
     );
 }  
