bde44327ba98b2bbd9bd0db068ee64e6692b0132
luiz <alvesluiz547@gmail.com>
2024-11-21 23:42:51 -0300

    cls em tudo ai

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from './pages/app'
 import Erro from './pages/erro'
-import Agendar from "./pages/agendar";
-import Login from "./pages/login";
-import Adm from "./pages/adm";
 import Adm2 from "./pages/adm2";
 
 export default function Navegação(){
@@ -13,9 +10,6 @@ export default function Navegação(){
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App/>} />
-                <Route path="/agendar" element={<Agendar/>} />
-                <Route path="/login" element={<Login/>} />
-                <Route path="/adm" element={<Adm/>} />
                 <Route path="/adm2" element={<Adm2/>} />
                 <Route path="*" element={<Erro/>} />
             </Routes>
